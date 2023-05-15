19629e837756a3e4161057478e1fbd72f6f116f4
Liyan Zhao <return65535@qq.com>
2023-05-15 13:31:14 +0800

    This is reden!

---
+++ src/main/java/com/github/zly2006/reden/Reden.java
@@ -0,0 +1,14 @@
+package com.github.zly2006.reden;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Reden implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("reden");
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
