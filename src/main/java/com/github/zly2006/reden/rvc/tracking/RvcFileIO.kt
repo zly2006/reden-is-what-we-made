deef4c2fbe0fa4c5907f3d0f3e9266d6323f0ede
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2023-08-25 16:39:50 -0400

    feat(RvcFileIO): Add some save/load funcs
    TrackPoint deserialization needs to be implemented.
    
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- src/main/java/com/github/zly2006/reden/rvc/tracking/RvcFileIO.kt
+++ src/main/java/com/github/zly2006/reden/rvc/tracking/RvcFileIO.kt
@@ -9,19 +9,14 @@ import net.minecraft.server.world.BlockEvent
 import net.minecraft.util.Identifier
 import net.minecraft.util.math.BlockPos
 import java.nio.file.Path
+import java.util.UUID
 
 object RvcFileIO: StructureIO {
-    private fun rvcFile(name: String) = "$name.rvc"
-
-    private fun writeRvcHeader(path: Path, name: String) {
-        path.resolve(rvcFile(name)).toFile().writeText(
-            "RVC; Version 1.0.0; Platform: MCMod/Reden; Data: $name\n"
-        )
-    }
+    private fun rvcFile(name: String): String = "$name.rvc"
+    private fun rvcHeader(name: String): String = "RVC; Version 1.0.0; Platform: MCMod/Reden; Data: $name\n"
 
     private fun writeRvcFile(path: Path, name: String, data: String) {
-        writeRvcHeader(path, name)
-        path.resolve(rvcFile(name)).toFile().writeText(data)
+        path.resolve(rvcFile(name)).toFile().writeText(rvcHeader(name) + data)
     }
 
     private fun readRvcHeader(path: Path, name: String) {
@@ -29,9 +24,10 @@ object RvcFileIO: StructureIO {
     }
 
     private fun readRvcFile(path: Path, name: String): List<String> {
-        readRvcHeader(path, name)
-        if (path.resolve(rvcFile("blockEvents")).toFile().exists()) {
-            return path.resolve(rvcFile(name)).toFile().readLines()
+        if (path.resolve(rvcFile(name)).toFile().exists()) {
+            readRvcHeader(path, name)
+            val lines = path.resolve(rvcFile(name)).toFile().readLines()
+            return lines.subList(1, lines.size)
         }
         return emptyList()
     }
@@ -45,44 +41,61 @@ object RvcFileIO: StructureIO {
         // ======================================== Save Blocks ========================================
         // public final val blocks: MutableMap<BlockPos, BlockState>
         // com.github.zly2006.reden.rvc.ReadWriteStructure
+        var blocksStr = ""
+        structure.blocks.forEach { (pos, state) ->
+            blocksStr += "${pos.x},${pos.y},${pos.z},${
+                NbtHelper.toNbtProviderString(NbtHelper.fromBlockState(state))
+            }\n"
+        }
+        writeRvcFile(path, "blocks", blocksStr)
 
         // ==================================== Save Block Entities ====================================
         // public final val blockEntities: MutableMap<BlockPos, NbtCompound>
         // com.github.zly2006.reden.rvc.ReadWriteStructure
+        var blockEntitiesStr = ""
+        structure.blockEntities.forEach { (pos, nbt) ->
+            blockEntitiesStr += "${pos.x},${pos.y},${pos.z},${NbtHelper.toNbtProviderString(nbt)}\n"
+        }
+        writeRvcFile(path, "blockEntities", blockEntitiesStr)
 
         // ======================================= Save Entities =======================================
         // public open val entities: MutableMap<UUID, NbtCompound>
         // com.github.zly2006.reden.rvc.ReadWriteStructure
+        var entitiesStr = ""
+        structure.entities.forEach { (uuid, nbt) ->
+            entitiesStr += "$uuid,${NbtHelper.toNbtProviderString(nbt)}\n"
+        }
+        writeRvcFile(path, "entities", entitiesStr)
 
         // ===================================== Save Track Points =====================================
         // public final val trackPoints: MutableList<TrackedStructure.TrackPoint>
         // com.github.zly2006.reden.rvc.tracking.TrackedStructure
-        val trackPointStr = structure.trackPoints.joinToString("\n") {
+        val trackPointsStr = structure.trackPoints.joinToString("\n") {
             "${it.pos.x},${it.pos.y},${it.pos.z},${it.predicate},${it.mode}"
         }
-        writeRvcFile(path, "trackPoints", trackPointStr)
+        writeRvcFile(path, "trackPoints", trackPointsStr)
 
         // ===================================== Save Block Events =====================================
         // public final val blockEvents: MutableList<BlockEvent>
         // com.github.zly2006.reden.rvc.tracking.TrackedStructure
-        val blockEventStr = structure.blockEvents.joinToString("\n") {
+        val blockEventsStr = structure.blockEvents.joinToString("\n") {
             "${it.pos.x},${it.pos.y},${it.pos.z},${it.type},${it.data},${Registries.BLOCK.getId(it.block)}"
         }
-        writeRvcFile(path, "blockEvents", blockEventStr)
+        writeRvcFile(path, "blockEvents", blockEventsStr)
 
         // ================================ Save Block Scheduled Ticks =================================
         // public final val blockScheduledTicks: MutableList<Tick<*>>
         // com.github.zly2006.reden.rvc.tracking.TrackedStructure
-        val blockScheduledTickStr = structure.blockScheduledTicks
+        val blockScheduledTicksStr = structure.blockScheduledTicks
             .joinToString("\n") { NbtHelper.toNbtProviderString(it) }
-        writeRvcFile(path, "blockScheduledTicks", blockScheduledTickStr)
+        writeRvcFile(path, "blockScheduledTicks", blockScheduledTicksStr)
 
         // ================================ Save Fluid Scheduled Ticks =================================
         // public final val fluidScheduledTicks: MutableList<Tick<*>>
         // com.github.zly2006.reden.rvc.tracking.TrackedStructure
-        val fluidScheduledTickStr = structure.fluidScheduledTicks
+        val fluidScheduledTicksStr = structure.fluidScheduledTicks
             .joinToString("\n") { NbtHelper.toNbtProviderString(it) }
-        writeRvcFile(path, "fluidScheduledTicks", fluidScheduledTickStr)
+        writeRvcFile(path, "fluidScheduledTicks", fluidScheduledTicksStr)
     }
 
     override fun load(path: Path, structure: IWritableStructure) {
@@ -95,21 +108,39 @@ object RvcFileIO: StructureIO {
         // public final val blocks: MutableMap<BlockPos, BlockState>
         // com.github.zly2006.reden.rvc.ReadWriteStructure
         structure.blocks.clear()
+        readRvcFile(path, "blocks").forEach {
+            val split = it.split(",")
+            structure.blocks[BlockPos(split[0].toInt(), split[1].toInt(), split[2].toInt())] = NbtHelper.toBlockState(
+                Registries.BLOCK.readOnlyWrapper,
+                NbtHelper.fromNbtProviderString(split[3])
+            )
+        }
 
         // ==================================== Load Block Entities ====================================
         // public final val blockEntities: MutableMap<BlockPos, NbtCompound>
         // com.github.zly2006.reden.rvc.ReadWriteStructure
         structure.blockEntities.clear()
+        readRvcFile(path, "blockEntities").forEach {
+            val split = it.split(",")
+            structure.blockEntities[BlockPos(split[0].toInt(), split[1].toInt(), split[2].toInt())] = NbtHelper.fromNbtProviderString(
+                split[3]
+            )
+        }
 
         // ======================================= Load Entities =======================================
         // public open val entities: MutableMap<UUID, NbtCompound>
         // com.github.zly2006.reden.rvc.ReadWriteStructure
         structure.entities.clear()
+        readRvcFile(path, "entities").forEach {
+            val split = it.split(",")
+            structure.entities[UUID.fromString(split[0])] = NbtHelper.fromNbtProviderString(split[1])
+        }
 
         // ===================================== Load Track Points =====================================
         // public final val trackPoints: MutableList<TrackedStructure.TrackPoint>
         // com.github.zly2006.reden.rvc.tracking.TrackedStructure
         structure.trackPoints.clear()
+        // TODO: TrackPoint deserialization needs to be implemented.
 
         // ===================================== Load Block Events =====================================
         // public final val blockEvents: MutableList<BlockEvent>
