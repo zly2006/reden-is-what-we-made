16c33706ee912e5c7f777a8ba3b35cd56e56d755
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2023-08-29 15:10:44 -0400

    feat: Complete RvcReader for version 1
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- src/main/java/com/github/zly2006/reden/rvc/tracking/reader/RvcReaderV1.kt
+++ src/main/java/com/github/zly2006/reden/rvc/tracking/reader/RvcReaderV1.kt
@@ -2,87 +2,91 @@ package com.github.zly2006.reden.rvc.tracking.reader
 
 import com.github.zly2006.reden.rvc.tracking.IRvcFileReader
 import com.github.zly2006.reden.rvc.tracking.RvcDataReader
-import com.github.zly2006.reden.rvc.tracking.RvcFileIO
 import com.github.zly2006.reden.rvc.tracking.TrackedStructure
 import net.minecraft.block.BlockState
 import net.minecraft.nbt.NbtCompound
 import net.minecraft.nbt.NbtHelper
+import net.minecraft.registry.Registries
 import net.minecraft.server.world.BlockEvent
+import net.minecraft.util.Identifier
 import net.minecraft.util.math.BlockPos
-import java.io.BufferedInputStream
 import java.util.*
 
 class RvcReaderV1(
     header: IRvcFileReader.RvcHeader
 ): IRvcFileReader {
     override fun readBlocksData(data: List<String>): Map<BlockPos, BlockState> {
-        TODO("Not yet implemented")
-//        readRvcFile(path, "blocks").forEach {
-//            val data = RvcDataReader(it, ",")
-//            val blockPos = BlockPos(data.readNext().toInt(), data.readNext().toInt(), data.readNext().toInt())
-//            val blockState = NbtHelper.toBlockState(
-//                Registries.BLOCK.readOnlyWrapper,
-//                NbtHelper.fromNbtProviderString(data.readGreedy())
-//            )
-//            structure.blocks[blockPos] = blockState
-//        }
+        val blocks = mutableMapOf<BlockPos, BlockState>()
+        data.forEach {
+            val rvcData = RvcDataReader(it, ",")
+            val blockPos = BlockPos(rvcData.readNext().toInt(), rvcData.readNext().toInt(), rvcData.readNext().toInt())
+            val blockState = NbtHelper.toBlockState(
+                Registries.BLOCK.readOnlyWrapper,
+                NbtHelper.fromNbtProviderString(rvcData.readGreedy())
+            )
+            blocks[blockPos] = blockState
+        }
+        return blocks
     }
 
     override fun readBlockEntitiesData(data: List<String>): Map<BlockPos, NbtCompound> {
-        TODO("Not yet implemented")
-//        readRvcFile(path, "blockEntities").forEach {
-//            val data = RvcDataReader(it, ",")
-//            val blockPos = BlockPos(data.readNext().toInt(), data.readNext().toInt(), data.readNext().toInt())
-//            val nbt = NbtHelper.fromNbtProviderString(data.readGreedy())
-//            structure.blockEntities[blockPos] = nbt
-//        }
+        val blockEntities = mutableMapOf<BlockPos, NbtCompound>()
+        data.forEach {
+            val rvcData = RvcDataReader(it, ",")
+            val blockPos = BlockPos(rvcData.readNext().toInt(), rvcData.readNext().toInt(), rvcData.readNext().toInt())
+            val nbt = NbtHelper.fromNbtProviderString(rvcData.readGreedy())
+            blockEntities[blockPos] = nbt
+        }
+        return blockEntities
     }
 
     override fun readEntitiesData(data: List<String>): Map<UUID, NbtCompound> {
-        TODO("Not yet implemented")
-//        readRvcFile(path, "entities").forEach {
-//            val data = RvcDataReader(it, ",")
-//            val uuid = UUID.fromString(data.readNext())
-//            val nbt = NbtHelper.fromNbtProviderString(data.readGreedy())
-//            structure.entities[uuid] = nbt
-//        }
-
+        val entities = mutableMapOf<UUID, NbtCompound>()
+        data.forEach {
+            val rvcData = RvcDataReader(it, ",")
+            val uuid = UUID.fromString(rvcData.readNext())
+            val nbt = NbtHelper.fromNbtProviderString(rvcData.readGreedy())
+            entities[uuid] = nbt
+        }
+        return entities
     }
 
     override fun readTrackPointData(data: List<String>): List<TrackedStructure.TrackPoint> {
-        TODO("Not yet implemented")
-//        structure.trackPoints.addAll(readRvcFile(path, "trackPoints").map {
-//            val data = RvcDataReader(it, ",")
-//            val blockPos = BlockPos(data.readNext().toInt(), data.readNext().toInt(), data.readNext().toInt())
-//            val predicate = data.readNext()
-//            val mode = data.readNext()
-//            TrackedStructure.TrackPoint(
-//                blockPos,
-//                TrackedStructure.TrackPoint.TrackPredicate.valueOf(predicate),
-//                TrackedStructure.TrackPoint.TrackMode.valueOf(mode)
-//            )
-//        })
+        val trackPoints = mutableListOf<TrackedStructure.TrackPoint>()
+        data.forEach {
+            val rvcData = RvcDataReader(it, ",")
+            val blockPos = BlockPos(rvcData.readNext().toInt(), rvcData.readNext().toInt(), rvcData.readNext().toInt())
+            val predicate = rvcData.readNext()
+            val mode = rvcData.readNext()
+            trackPoints.add(
+                TrackedStructure.TrackPoint(
+                    blockPos,
+                    TrackedStructure.TrackPoint.TrackPredicate.valueOf(predicate),
+                    TrackedStructure.TrackPoint.TrackMode.valueOf(mode)
+                )
+            )
+        }
+        return trackPoints
     }
 
     override fun readBlockEventsData(data: List<String>): List<BlockEvent> {
-        TODO("Not yet implemented")
-//        readRvcFile(path, "blockEvents").forEach {
-//            val split = it.split(",")
-//            structure.blockEvents.add(
-//                BlockEvent(
-//                    BlockPos(split[0].toInt(), split[1].toInt(), split[2].toInt()),
-//                    Registries.BLOCK.get(Identifier(split[5])),
-//                    split[3].toInt(),
-//                    split[4].toInt()
-//                )
-//            )
-//        }
+        val blockEvents = mutableListOf<BlockEvent>()
+        data.forEach {
+            val rvcData = RvcDataReader(it, ",")
+            val blockPos = BlockPos(rvcData.readNext().toInt(), rvcData.readNext().toInt(), rvcData.readNext().toInt())
+            val type = rvcData.readNext().toInt()
+            val data = rvcData.readNext().toInt()
+            val block = Registries.BLOCK.get(Identifier(rvcData.readGreedy()))
+            blockEvents.add(BlockEvent(blockPos, block, type, data))
+        }
+        return blockEvents
     }
 
     override fun readScheduledTicksData(data: List<String>): List<NbtCompound> {
-        TODO("Not yet implemented")
-//        readRvcFile(path, "blockScheduledTicks").forEach {
-//            structure.xxxScheduledTicks.add(NbtHelper.fromNbtProviderString(it))
-//        }
+        val scheduledTicks = mutableListOf<NbtCompound>()
+        data.forEach {
+            scheduledTicks.add(NbtHelper.fromNbtProviderString(it))
+        }
+        return scheduledTicks
     }
 }
