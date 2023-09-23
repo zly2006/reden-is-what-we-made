2d50f66c4a79635639b335d93926a4bcf044ddff
Liyan Zhao <return65535@qq.com>
2023-09-23 14:31:56 +0800

    fix

---
+++ mineroutine/src/main/java/com/redenmc/mineroutine/mixin/update/Mixin119Update.java
@@ -0,0 +1,8 @@
+package com.redenmc.mineroutine.mixin.update;
+
+import net.minecraft.world.block.ChainRestrictedNeighborUpdater;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ChainRestrictedNeighborUpdater.class)
+public class Mixin119Update {
+}
