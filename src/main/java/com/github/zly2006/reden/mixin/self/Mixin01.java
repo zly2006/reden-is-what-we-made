92fc8aa138ff57be29bf45ca40ee794432ae8cbd
Liyan Zhao <return65535@qq.com>
2023-10-12 14:00:33 +0800

    refactor: stop using kotlin coroutines

--- src/main/java/com/github/zly2006/reden/mixin/self/Mixin01.java
+++
@@ -1,7 +0,0 @@
-package com.github.zly2006.reden.mixin.self;
-
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(targets = "com/github/zly2006/reden/transformers/CoroutinesKt$startCoroutineScope$1")
-public class Mixin01 {
-}
