ee85d9dd6a83569c7f3f58d01cd806289797c9ab
Liyan Zhao <return65535@qq.com>
2023-11-18 16:55:25 +0800

    chore

--- src/main/java/com/github/zly2006/reden/mixin/privacy/MixinLanguageManager.java
+++ src/main/java/com/github/zly2006/reden/mixin/privacy/MixinLanguageManager.java
@@ -9,15 +9,15 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-// For some reason idk, i18n is not initialized when the game starts, so we have to wait for it to be initialized
 @Mixin(LanguageManager.class)
 public class MixinLanguageManager {
     @Inject(
             method = "reload",
             at = @At("RETURN")
     )
-    private void reloadPrivacyLanguage(ResourceManager manager, CallbackInfo ci) {
+    // For some reason idk, i18n is not initialized when the game starts,
+    // so we have to wait for it to be initialized
+    private void reloadLanguage(ResourceManager manager, CallbackInfo ci) {
         MinecraftClient client = MinecraftClient.getInstance();
         if (!MalilibSettingsKt.iPRIVACY_SETTING_SHOWN.getBooleanValue()) {
             client.setScreen(new PrivacyScreen(client.currentScreen));
--- src/main/java/com/github/zly2006/reden/utils/BlockUtils.kt
+++
@@ -1,8 +0,0 @@
-package com.github.zly2006.reden.utils
-
-import net.minecraft.block.BlockState
-
-val BlockState.isDepending: Boolean
-    get() {
-        return false
-    }
