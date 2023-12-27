f71ac381e7cb0aeb0abf705f5e9087eca13ea05a
Liyan Zhao <return65535@qq.com>
2023-12-27 20:34:45 +0800

    feat
    interface for #74

---
+++ src/main/java/com/github/zly2006/reden/gui/message/MessageQueue.kt
@@ -0,0 +1,28 @@
+package com.github.zly2006.reden.gui.message
+
+import net.minecraft.text.Text
+import net.minecraft.util.Identifier
+
+object ClientMessageQueue {
+    class Button(
+        val message: Text,
+        val action: () -> Unit
+    )
+    data class Message(
+        val icon: Identifier,
+        val title: Text,
+        val body: Text,
+        val buttons: List<Button>
+    )
+    val messages: MutableList<Message> = mutableListOf()
+    fun add(icon: Identifier, title: Text, body: Text, buttons: List<Button>): Int {
+        TODO()
+    }
+    fun remove(id: Int) {
+        messages.removeAt(id)
+        // todo: refresh screen
+    }
+    fun openScreen() {
+        TODO()
+    }
+}
