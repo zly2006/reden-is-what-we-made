3a98732ed40e82a70af7fdaaeec44f58cd0a253e
Liyan Zhao <return65535@qq.com>
2024-01-30 12:36:16 +0800

    fix
    Signed-off-by: Liyan Zhao <return65535@qq.com>

--- src/main/java/com/github/zly2006/reden/mixin/forceSyncEntityPos/MixinWorldRenderer.java
+++ src/main/java/com/github/zly2006/reden/mixin/forceSyncEntityPos/MixinWorldRenderer.java
@@ -3,13 +3,12 @@ package com.github.zly2006.reden.mixin.forceSyncEntityPos;
 import com.github.zly2006.reden.malilib.MalilibSettingsKt;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldRenderer.class)
@@ -27,18 +26,17 @@ public class MixinWorldRenderer {
         this.cameraY = cameraY;
         this.cameraZ = cameraZ;
     }
-    @Redirect(
+
+    @ModifyVariable(
             method = "renderEntity",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/render/entity/EntityRenderDispatcher;render(Lnet/minecraft/entity/Entity;DDDFFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V"
-            )
+            at = @At("HEAD"),
+            ordinal = 0,
+            argsOnly = true
     )
-    private <E extends Entity> void render(EntityRenderDispatcher dispatcher, E entity, double x, double y, double z, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light) {
+    private float modifyDelta(float tickDelta) {
         if (MalilibSettingsKt.TOGGLE_FORCE_ENTITY_POS_SYNC.getBooleanValue()) {
-            dispatcher.render(entity, entity.getX() - cameraX, entity.getY() - cameraY, entity.getZ() - cameraZ, yaw, tickDelta, matrices, vertexConsumers, light);
-        } else {
-            dispatcher.render(entity, x, y, z, yaw, tickDelta, matrices, vertexConsumers, light);
+            return 1f; // the real position
         }
+        return tickDelta;
     }
 }
--- src/main/java/com/github/zly2006/reden/mixin/rvc/MixinWorldChunk.java
+++ src/main/java/com/github/zly2006/reden/mixin/rvc/MixinWorldChunk.java
@@ -1,10 +1,12 @@
 package com.github.zly2006.reden.mixin.rvc;
 
 import com.github.zly2006.reden.access.ClientData;
+import com.github.zly2006.reden.rvc.tracking.RvcRepository;
 import com.github.zly2006.reden.rvc.tracking.WorldInfo;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.WorldChunk;
 import org.spongepowered.asm.mixin.Final;
@@ -13,6 +15,8 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Arrays;
 
 import static com.github.zly2006.reden.access.ClientData.getData;
 
@@ -24,16 +28,16 @@ public abstract class MixinWorldChunk {
 
     @Inject(
             method = "setBlockState",
-            at = @At("HEAD")
+            at = @At("TAIL")
     )
-    private void onBlockChange(BlockPos pos, BlockState state, boolean moved, CallbackInfoReturnable<BlockState> cir) {
+    private void onBlockChanged(BlockPos pos, BlockState state, boolean moved, CallbackInfoReturnable<BlockState> cir) {
         //todo
         if (world.isClient) {
             ClientData data = getData(MinecraftClient.getInstance());
             WorldInfo worldInfo = WorldInfo.Companion.getWorldInfo(MinecraftClient.getInstance());
-            data.getRvcStructures().values().forEach(repo -> {
-                if (repo.getPlacementInfo() == null) return;
-                if (!worldInfo.equals(repo.getPlacementInfo().getWorldInfo())) return;
+            for (RvcRepository repo : data.getRvcStructures().values()) {
+                if (repo.getPlacementInfo() == null) continue;
+                if (!worldInfo.equals(repo.getPlacementInfo().getWorldInfo())) continue;
                 var structure = repo.head();
                 if (structure.isInArea(structure.getRelativeCoordinate(pos))) {
                     if (state.isAir()) {
@@ -41,8 +45,10 @@ public abstract class MixinWorldChunk {
                     } else {
                         structure.onBlockAdded(pos);
                     }
+                } else if (Arrays.stream(Direction.values()).anyMatch(dir -> structure.isInArea(structure.getRelativeCoordinate(pos.offset(dir))))) {
+                    structure.onBlockAdded(pos);
                 }
-            });
+            }
         }
     }
 }
