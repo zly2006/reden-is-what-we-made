4229c7d8e6ad0a4a8430abbe6ee7163d9f2693b0
Liyan Zhao <return65535@qq.com>
2024-01-31 00:39:48 +0800

    perf: use vertex buffer array to render BlockOutline
    Signed-off-by: Liyan Zhao <return65535@qq.com>
    
    #73

--- src/main/java/com/github/zly2006/reden/render/BlockOutline.kt
+++ src/main/java/com/github/zly2006/reden/render/BlockOutline.kt
@@ -1,18 +1,85 @@
 package com.github.zly2006.reden.render
 
 import com.github.zly2006.reden.malilib.FANCY_BLOCK_OUTLINE
+import io.netty.util.concurrent.CompleteFuture
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
+import net.minecraft.block.BlockRenderType
 import net.minecraft.block.BlockState
 import net.minecraft.client.MinecraftClient
-import net.minecraft.client.render.Camera
-import net.minecraft.client.render.OverlayTexture
-import net.minecraft.client.render.VertexConsumer
+import net.minecraft.client.render.*
+import net.minecraft.client.render.BufferBuilder.BuiltBuffer
+import net.minecraft.client.render.chunk.BlockBufferBuilderStorage
 import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.random.Random
 
 @Environment(EnvType.CLIENT)
 object BlockOutline {
+    fun prepareUploadForLayer(futureList: MutableList<CompleteFuture<Void>>) {
+
+    }
+
+    /**
+     * From: [net.minecraft.client.render.chunk.ChunkBuilder.BuiltChunk.RebuildTask.render]
+     *
+     * todo: [net.minecraft.client.render.chunk.ChunkBuilder.scheduleUpload]
+     *
+     * todo: bufferStorage, see vanilla, is that the same builders as immediate?
+     */
+    fun render(bufferStorage: BlockBufferBuilderStorage) {
+        val mc = MinecraftClient.getInstance()
+        val nonNullLayers = mutableSetOf<RenderLayer>()
+        val blockRenderManager = mc.blockRenderManager
+
+        val uploadFutures = mutableListOf<CompleteFuture<Void>>()
+        val matrixStack = MatrixStack()
+        val random = Random.create()
+        blocks.forEach { (pos, state) ->
+            if (!state.fluidState.isEmpty) {
+                val renderLayer = RenderLayers.getFluidLayer(state.fluidState)
+                val builder = bufferStorage.get(renderLayer)
+                if (nonNullLayers.add(renderLayer)) {
+                    prepareUploadForLayer(uploadFutures)
+                }
+                // we dont have the chunkRendererRegion, so we use mc.world
+                blockRenderManager.renderFluid(pos, mc.world, builder, state, state.fluidState)
+            }
+            if (state.renderType == BlockRenderType.INVISIBLE) return@forEach
+            val renderLayer = RenderLayers.getBlockLayer(state)
+            val bufferBuilder = bufferStorage.get(renderLayer)
+            if (nonNullLayers.add(renderLayer)) {
+                prepareUploadForLayer(uploadFutures)
+            }
+            matrixStack.push()
+            matrixStack.translate(pos.x.toDouble(), pos.y.toDouble(), pos.z.toDouble())
+            blockRenderManager.renderBlock(
+                state,
+                pos,
+                mc.world,
+                matrixStack,
+                bufferBuilder,
+                true,
+                random
+            )
+            matrixStack.pop()
+        }
+// Vanilla:
+//    we are rendering outline, so sorting doesn't matter
+//
+//        if (set.contains(RenderLayer.getTranslucent()) && !(bufferBuilder2 = storage.get(RenderLayer.getTranslucent())).isBatchEmpty()) {
+//            bufferBuilder2.setSorter(VertexSorter.byDistance(cameraX - (float)blockPos.getX(), cameraY - (float)blockPos.getY(), cameraZ - (float)blockPos.getZ()));
+//            renderData.translucencySortingData = bufferBuilder2.getSortingData();
+//        }
+
+        // build vbo
+        val builtBuffers = mutableMapOf<RenderLayer, BuiltBuffer>()
+        for (layer in nonNullLayers) {
+            val builtBuffer = bufferStorage.get(layer).endNullable() ?: continue
+            builtBuffers[layer] = builtBuffer
+        }
+    }
+
     fun render(buffer: VertexConsumer, matrices: MatrixStack, camera: Camera) {
         val mc = MinecraftClient.getInstance()
         val renderManager = mc.blockRenderManager
