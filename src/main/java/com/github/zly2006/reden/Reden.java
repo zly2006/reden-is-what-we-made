f6d36defcc6f07bcb1f94da13f8a762bd09481e6
Liyan Zhao <return65535@qq.com>
2024-03-13 19:58:51 +0800

    fix: #98, dont register reden sounds so they can join remote servers.
    Signed-off-by: Liyan Zhao <return65535@qq.com>

--- src/main/java/com/github/zly2006/reden/Reden.java
+++ src/main/java/com/github/zly2006/reden/Reden.java
@@ -162,7 +162,6 @@ public class Reden implements ModInitializer, CarpetExtension {
                 LOGGER.info("This is Reden!");
             }
         });
-        Sounds.init();
         ServerTickEvents.END_SERVER_TICK.register(TaskScheduler.INSTANCE);
     }
 
