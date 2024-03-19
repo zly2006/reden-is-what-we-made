f4f0e8a78a21f989dd1531dd5c05328d58e134d3
Liyan Zhao <return65535@qq.com>
2024-03-19 14:04:24 +0800

    fix
    Signed-off-by: Liyan Zhao <return65535@qq.com>

--- src/main/java/com/github/zly2006/reden/rvc/gui/hud/gameplay/SelectModeHud.kt
+++ src/main/java/com/github/zly2006/reden/rvc/gui/hud/gameplay/SelectModeHud.kt
@@ -1,6 +1,5 @@
 package com.github.zly2006.reden.rvc.gui.hud.gameplay
 
-import com.github.zly2006.reden.Reden
 import com.github.zly2006.reden.rvc.gui.RvcHudRenderer
 import com.github.zly2006.reden.rvc.gui.selectedStructure
 import com.github.zly2006.reden.rvc.tracking.TrackPredicate
@@ -8,11 +7,6 @@ import com.github.zly2006.reden.utils.holdingToolItem
 import com.github.zly2006.reden.utils.red
 import net.minecraft.client.MinecraftClient
 import net.minecraft.text.Text
-import net.minecraft.util.Formatting
-import net.minecraft.util.Identifier
-
-
-val hudId = Identifier(Reden.MOD_ID, "select_mode_hud")
 
 fun registerHud() {
     RvcHudRenderer.supplierMap["select_mode_hud"] = {
@@ -24,15 +18,10 @@ fun registerHud() {
             }
             else {
                 list.add(Text.translatable("reden.widget.rvc.hud.selected", selectedStructure!!.name))
-                val trackCount = selectedStructure!!.trackPoints.count { it.mode == TrackPredicate.TrackMode.TRACK }
-                val ignoreCount = selectedStructure!!.trackPoints.count { it.mode == TrackPredicate.TrackMode.IGNORE }
-                list.add(
-                    Text.translatable("reden.widget.rvc.hud.trackpoints")
-                        .append(" ")
-                        .append(Text.translatable("reden.widget.rvc.hud.trackpoints.tracking", trackCount).formatted(Formatting.GREEN))
-                        .append(" ")
-                        .append(Text.translatable("reden.widget.rvc.hud.trackpoints.ignoring", ignoreCount).formatted(Formatting.RED))
-                )
+                val allTrackpoint = selectedStructure!!.regions.values.flatMap { it.trackPoints }
+                val trackCount = allTrackpoint.count { it.mode == TrackPredicate.TrackMode.TRACK }
+                val ignoreCount = allTrackpoint.count { it.mode == TrackPredicate.TrackMode.IGNORE }
+                list.add(Text.translatable("reden.widget.rvc.hud.trackpoints", trackCount, ignoreCount))
                 if (selectedStructure!!.placementInfo == null) {
                     list.add(Text.literal("[Warning] Not placed").red())
                 }
