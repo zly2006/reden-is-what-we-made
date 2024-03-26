3d1e915056aca0b7abb072d861581e2be082e760
Liyan Zhao <return65535@qq.com>
2024-03-26 12:28:50 +0800

    feat: some cli
    Signed-off-by: Liyan Zhao <return65535@qq.com>

--- src/main/java/com/github/zly2006/reden/rvc/RvcLocalCommand.kt
+++ src/main/java/com/github/zly2006/reden/rvc/RvcLocalCommand.kt
@@ -2,11 +2,13 @@ package com.github.zly2006.reden.rvc
 
 import com.github.zly2006.reden.access.ClientData.Companion.data
 import com.github.zly2006.reden.rvc.gui.selectedRepository
+import com.github.zly2006.reden.rvc.tracking.WorldInfo.Companion.getWorldInfo
 import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.arguments.ArgumentType
 import com.mojang.brigadier.arguments.StringArgumentType.*
 import com.mojang.brigadier.builder.ArgumentBuilder
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
+import com.mojang.brigadier.builder.RequiredArgumentBuilder
 import com.mojang.brigadier.context.CommandContext
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager
@@ -26,9 +28,13 @@ fun register(dispatcher: CommandDispatcher<FabricClientCommandSource>) {
     val mc = MinecraftClient.getInstance()
     dispatcher.register(ClientCommandManager.literal("rvc-local").then {
         literal("use").then {
-            argument("name", string()).executes {
+            argument("name", string()).suggests(mc.data.rvcStructures.keys).executes {
                 val name = it.string("name")
                 selectedRepository = mc.data.rvcStructures[name] ?: error("No such repository: $name")
+                if (selectedRepository!!.placementInfo?.worldInfo != mc.getWorldInfo()) {
+                    selectedRepository = null
+                    error("Repository $name is not in the current world")
+                }
                 1
             }
         }
@@ -69,7 +75,7 @@ fun register(dispatcher: CommandDispatcher<FabricClientCommandSource>) {
                 1
             }
             literal("remove").then {
-                argument("name", string()).executes {
+                argument("name", string()).suggests(selectedRepository!!.head().regions.keys).executes {
                     selectedRepository ?: error("No repository selected")
                     val name = it.string("name")
                     Git.open(selectedRepository!!.git.repository.workTree.resolve(name))
@@ -118,7 +124,21 @@ fun register(dispatcher: CommandDispatcher<FabricClientCommandSource>) {
             }
             1
         }
-        literal("log")
+        literal("log").executes { context ->
+            selectedRepository ?: error("No repository selected")
+            selectedRepository!!.git.log().call().forEach {
+                context.source.sendMessage(
+                    "${
+                        it.name.substring(
+                            0,
+                            7
+                        )
+                    }: ${it.shortMessage} at ${it.commitTime} by ${it.authorIdent.name}"
+                )
+            }
+            literal("s")
+            1
+        }
         literal("checkout")
         literal("branch").then {
             literal("ls").executes { context ->
@@ -141,6 +161,11 @@ fun register(dispatcher: CommandDispatcher<FabricClientCommandSource>) {
     } as LiteralArgumentBuilder<FabricClientCommandSource>)
 }
 
+private fun RequiredArgumentBuilder<*, *>.suggests(keys: Collection<String>) = suggests { _, builder ->
+    keys.forEach { builder.suggest(it) }
+    builder.buildFuture()
+}
+
 private fun FabricClientCommandSource.sendMessage(s: String) {
     sendFeedback(Text.literal(s))
 }
@@ -154,14 +179,15 @@ class BuilderScope<S>(
     val builders: MutableList<ArgumentBuilder<S, *>> = mutableListOf()
 ) {
     @CommandBuilder
-    fun literal(name: String) = ClientCommandManager.literal(name)!!
+    fun literal(name: String) = LiteralArgumentBuilder.literal<S>(name)!!.also(builders::add)
 
     @CommandBuilder
-    fun argument(name: String, type: ArgumentType<*>) = ClientCommandManager.argument(name, type)!!
+    fun <T> argument(name: String, type: ArgumentType<T>) =
+        RequiredArgumentBuilder.argument<S, T>(name, type)!!.also(builders::add)
 }
 
 private fun <S> ArgumentBuilder<S, *>.then(function: BuilderScope<S>.() -> Unit) = apply {
     val scope = BuilderScope(this)
     function(scope)
-    scope.builders.forEach(::then)
+    scope.builders.forEach(this::then)
 }
