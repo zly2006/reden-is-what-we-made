55ff571bba4e962a121969cad90f2a450e6e6c01
Liyan Zhao <return65535@qq.com>
2024-04-02 19:04:08 +0800

    feat
    Signed-off-by: Liyan Zhao <return65535@qq.com>

--- src/main/java/com/github/zly2006/reden/yo/Greenstone.kt
+++ src/main/java/com/github/zly2006/reden/yo/Greenstone.kt
@@ -8,18 +8,27 @@ import net.minecraft.client.MinecraftClient
 import java.util.*
 
 fun registerGreenstone() {
-    if (!ResourceManagerHelper.registerBuiltinResourcePack(
-            Reden.identifier("greenstone"),
-            FabricLoader.getInstance().getModContainer(Reden.MOD_ID).get(),
-            ResourcePackActivationType.NORMAL
-        )
-    ) {
-        Reden.LOGGER.error("Failed to register greenstone resource pack")
+    val packs = listOf(
+        Reden.identifier("greenstone"),
+        Reden.identifier("communist_redstone")
+    )
+    packs.forEach {
+        if (!ResourceManagerHelper.registerBuiltinResourcePack(
+                it, FabricLoader.getInstance().getModContainer(Reden.MOD_ID).get(), ResourcePackActivationType.NORMAL
+            )
+        ) {
+            Reden.LOGGER.error("Failed to register $it resource pack")
+        }
     }
 
     if (Calendar.getInstance()[Calendar.MONTH] == Calendar.APRIL
         && Calendar.getInstance()[Calendar.DAY_OF_MONTH] == 1
     ) {
         MinecraftClient.getInstance().resourcePackManager.enable("reden:greenstone")
+    }
+    if (Calendar.getInstance()[Calendar.MONTH] == Calendar.MARCH
+        && Calendar.getInstance()[Calendar.DAY_OF_MONTH] == 28
+    ) {
+        MinecraftClient.getInstance().resourcePackManager.enable("reden:communist_redstone")
     }
 }
