b93827be20631d9cbb60f74ee1446c4245e5056a
Liyan Zhao <return65535@qq.com>
2025-05-30 23:05:32 +0800

    sc 0.7

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,11 +3,15 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
         maven("https://maven.fabricmc.net/")
+        maven {
+            name = "Kikugie's Maven"
+            url = uri("https://maven.kikugie.dev/snapshots")
+        }
     }
 }
 
 plugins {
-    id("dev.kikugie.stonecutter") version "0.6"
+    id("dev.kikugie.stonecutter") version "0.7-alpha.18"
 }
 
 stonecutter {
--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -6,21 +6,7 @@ plugins {
     //id("dev.kikugie.j52j") version "1.0.2" apply false // Enables asset processing by writing json5 files
     id("me.modmuss50.mod-publish-plugin") version "0.7.+" apply false
 }
-stonecutter active "1.21.5" /* [SC] DO NOT EDIT */
-
-// Builds every version into `build/libs/{mod.version}/`
-stonecutter registerChiseled tasks.register("chiseledBuild", stonecutter.chiseled) {
-    group = "project"
-    ofTask("buildAndCollect")
-}
-
-/*
-// Publishes every version
-stonecutter registerChiseled tasks.register("chiseledPublishMods", stonecutter.chiseled) {
-    group = "project"
-    ofTask("publishMods")
-}
-*/
+stonecutter active "1.21.1" /* [SC] DO NOT EDIT */
 
 stonecutter parameters {
     /*
@@ -28,10 +14,14 @@ stonecutter parameters {
     and https://stonecutter.kikugie.dev/
     */
     // Swaps replace the scope with a predefined value
-    swap("mod_version", "\"${property("mod.version")}\";")
-    // Constants add variables available in conditions
-    const("release", property("mod.id") != "template")
-    // Dependencies add targets to check versions against
-    // Using `node.property()` in this block gets the versioned property
-    dependency("fapi", node!!.project.property("deps.fabric_api").toString())
+    swaps {
+        put("mod_version") {
+            "\"${property("mod.version")}\";"
+        }
+    }
+    dependencies {
+        put("fapi") {
+            node.project.property("deps.fabric_api").toString()
+        }
+    }
 }
