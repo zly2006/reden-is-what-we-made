ca8c271b78cdd01891ee4bd221c2538d7aaeea25
Liyan Zhao <return65535@qq.com>
2025-07-14 17:35:41 +0800

    fix
    #134

--- src/client/java/com/github/zly2006/reden/mixin/client/MixinGuiSchematicLoad.java
+++ src/client/java/com/github/zly2006/reden/mixin/client/MixinGuiSchematicLoad.java
@@ -6,6 +6,7 @@ import com.llamalad7.mixinextras.sugar.Local;
 import fi.dy.masa.litematica.gui.GuiSchematicBrowserBase;
 import fi.dy.masa.litematica.gui.GuiSchematicLoad;
 import fi.dy.masa.malilib.gui.button.ButtonGeneric;
+import net.minecraft.client.gui.screens.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -23,10 +24,20 @@ public abstract class MixinGuiSchematicLoad extends GuiSchematicBrowserBase {
             at = @At(value = "TAIL")
     )
     private void addButton(CallbackInfo ci, @Local(ordinal = 0) int x, @Local(ordinal = 1) int y) {
-        var label = "Download";
-        int buttonWidth = this.getStringWidth(label) + 10;
-        x -= buttonWidth + 4;
-        var button = new ButtonGeneric(x, y, buttonWidth, 20, label);
-        this.addButton(button, (button1, mouseButton) -> mc.setScreen(new MevScreen()));
+        try {
+            var label = "Download";
+            int buttonWidth = this.getStringWidth(label) + 10;
+            x -= buttonWidth + 4;
+            var button = new ButtonGeneric(x, y, buttonWidth, 20, label);
+            @SuppressWarnings("unchecked")
+            Class<Screen> clazz = (Class<Screen>) Class.forName("com.github.zly2006.reden.webmatic.MevScreen");
+            this.addButton(button, (button1, mouseButton) -> {
+                try {
+                    mc.setScreen(clazz.getConstructor().newInstance());
+                } catch (Throwable ignored) {
+                }
+            });
+        } catch (Throwable ignored) {
+        }
     }
 }
